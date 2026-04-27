e2ab560cab89344616655cc8397f71eb3c3127fe
HanSeungHee2 <tmdgml88888@gamil.com>
2026-04-27 23:06:51 +0900

    first upload

---
+++ Pointer(문자열 형식의 포인터 출력방식 연습).cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+
+	char str[] = "Hello";
+	char *str_ptr = str;
+
+	std::cout << "str_ptr: " << str_ptr << std::endl; //str_ptr: 배열주소 
+	//배열 주소가 나오지않는다. str_ptr은 시작메모리주소를 저장하는 포인터변수지만 char *형식의 데이터이다.
+	//컴파일러에서 std::cout으로 char형식 포인터 변수를 출력할때는 문자열로 출력한다. 즉 시작 메모리주소를 보고
+	// /0(끝 신호)가 나올때까지 출력하는 거로 해석한다.
+	//즉 형변환이 필요한데 이는 (변경 형식)str_ptr로 하면된다.
+
+	//ex:
+
+	std::cout << "void* str_ptr: " << (void*)str_ptr << std::endl; //str_ptr: 배열주소
+
+	std::cout << "*str_ptr: " << *str_ptr << std::endl; //*str_ptr: H (맞음. 
+	// str_ptr은 str의 시작메모리를 저장하는 포인터변수임 이를 역참조하면 시작문자인 H가 나온다.)
+	
+
+	int int_val = 10;
+	int* int_ptr = &int_val;
+	
+	std::cout << "int_ptr: " << int_ptr << std::endl; //int_ptr: 배열주소 (맞다.)
+	std::cout << "*int_ptr: " << *int_ptr << std::endl; //*int_ptr: 10 (맞다.)
+
+}
---
+++ 문제 7-2 1차 구현 코드..cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <map>
+#include <array>
+#include <numbers>
+using namespace std;
+
+class player {
+public:
+	player() {};
+	player(int h, int p, int s) : health(h), power(p), speed(s) {};
+
+	//멤버 변수 초기화 함수.(자식 클래스에서 사용)
+	void set_health(int h) {
+		health = h;}
+	void set_power(int p) {
+		power = p;}
+	void set_speed(int s) {
+		speed = s;}
+
+	//멤버 변수 반환 함수. (확인용)
+	int get_health() {
+		return health;
+	}
+	int get_power() {
+		return power;
+	}
+	int get_speed() {
+		return speed;
+	}
+
+private:
+	int health;
+	int power;
+	int speed;
+};
+
+//포수 기본 클래스.
+class catcher : public player {
+public:
+	catcher() {};
+
+	void ball_catch();
+};
+
+void catcher::ball_catch() {
+	std::cout << "공을 잡습니다." << std::endl;
+}
+
+//투수 기본 클래스.
+class pitcher : public player {
+public:
+	pitcher(int b_s) : ball_speed(b_s) {};
+
+	void set_ball_speed(int b_s) {
+		ball_speed = b_s;
+	}
+	int get_ball_speed() {
+		return ball_speed;
+	}
+
+	void ball_throw(catcher& our_catcher);
+
+private:
+	int ball_speed;
+};
+
+void pitcher::ball_throw(catcher& our_catcher) {
+	std::cout << "공을 던집니다." << std::endl;
+}
+
+
+
+//타자 기본 클래스.
+class batter : public player {
+public:
+	batter() {};
+
+	void ball_hit();
+};
+
+void batter::ball_hit() {
+	std::cout << "공을 칩니다." << std::endl;
+}
+
+//코치 기본 클래스.
+class coach : public player {
+public:
+	coach() {};
+
+	void coaching();
+};
+
+void coach::coaching() {
+	std::cout << "지시를 내립니다." << std::endl;
+}
+
+int main() {
+	coach our_coach;
+
+	pitcher our_pitcher(150);
+	batter our_batter;
+	catcher our_catcher;
+
+	//멤버 기능 확인
+	std::cout << "우리팀 감독이" << " ";
+	our_coach.coaching();
+	std::cout << std::endl;
+
+	std::cout << "우리팀 투수가" << " 우리팀 포수에게 " << " ";
+	our_pitcher.ball_throw(our_catcher);
+	std::cout << std::endl;
+
+	std::cout << "우리팀 포수가" << " ";
+	our_catcher.ball_catch();
+	std::cout << std::endl;
+
+	std::cout << "구속은 " << our_pitcher.get_ball_speed() << "km 입니다." << std::endl;
+
+	std::cout << "우리팀 타자가" << " ";
+	our_batter.ball_hit();
+	std::cout << std::endl;
+}
